30ca306660187670ffac407c94074b2fcc4774cb
Григорий Толмачев <tolmachev.gm@phystech.edu>
2024-11-05 04:20:14 +0700

    new scripts

--- episode 1/main.py
+++ episode 1/main.py
@@ -1,51 +1,13 @@
 import matplotlib.pyplot as plt
-with open ("001.dat",'r') as f:
-    s=f.read().split('\n')
-    x=[]
-    y=[]
-    for i in range(int(s[0])):
-        x.append(float(s[i+1].split()[0]))
-        y.append(float(s[i+1].split()[1]))
-    plt.plot(x,y,marker='o',linestyle='')
-    plt.savefig("1.png")
-    plt.show()
-with open ("002.dat",'r') as f:
-    s=f.read().split('\n')
-    x=[]
-    y=[]
-    for i in range(int(s[0])):
-        x.append(float(s[i+1].split()[0]))
-        y.append(float(s[i+1].split()[1]))
-    plt.plot(x,y,marker='o',linestyle='')
-    plt.savefig("2.png")
-    plt.show()
-with open ("003.dat",'r') as f:
-    s=f.read().split('\n')
-    x=[]
-    y=[]
-    for i in range(int(s[0])):
-        x.append(float(s[i+1].split()[0]))
-        y.append(float(s[i+1].split()[1]))
-    plt.plot(x,y,marker='o',linestyle='')
-    plt.savefig("3.png")
-    plt.show()
-with open ("004.dat",'r') as f:
-    s=f.read().split('\n')
-    x=[]
-    y=[]
-    for i in range(int(s[0])):
-        x.append(float(s[i+1].split()[0]))
-        y.append(float(s[i+1].split()[1]))
-    plt.plot(x,y,marker='o',linestyle='')
-    plt.savefig("4.png")
-    plt.show()
-with open ("005.dat",'r') as f:
-    s=f.read().split('\n')
-    x=[]
-    y=[]
-    for i in range(int(s[0])):
-        x.append(float(s[i+1].split()[0]))
-        y.append(float(s[i+1].split()[1]))
-    plt.plot(x,y,marker='o',linestyle='')
-    plt.savefig("5.png")
-    plt.show()
+for j in range(5):
+    g=str(j+1)
+    with open ("00"+g+".dat",'r') as f:
+        s=f.read().split('\n')
+        x=[]
+        y=[]
+        for i in range(int(s[0])):
+            x.append(float(s[i+1].split()[0]))
+            y.append(float(s[i+1].split()[1]))
+        plt.plot(x,y,marker='o',linestyle='')
+        plt.savefig(g+".png")
+        plt.show()
---
+++ episode 2/main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+with open ("frames.dat.txt",'r') as f:
+    s=f.read().split('\n')
+
+    for i in range (6):
+        x=[]
+        y=[]
+        print(s[2*i])
+        for a in range(101):
+            x.append(float(s[2*i].split()[a]))
+            print(a)
+        for a in range(101):
+            y.append(float(s[2*i+1].split()[a]))
+        qw=str(i+1)
+        print(qw)
+        plt.plot(x,y)
+        plt.grid()
+        plt.savefig(qw+".png")
+        plt.show()
